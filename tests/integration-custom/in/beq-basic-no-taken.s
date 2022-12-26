8baa81a85ab7e732b475a6d7fa4dcde46ad8a985
huzhi <hu.zhi@outlook.com>
2022-12-26 23:09:12 +0800

    part b, task 7

---
+++ tests/integration-custom/in/beq-basic-no-taken.s
@@ -0,0 +1,9 @@
+         addi s1, x0, 1
+         addi s0, x0, -1
+         beq  x0, s1, label1
+         addi s0, s0, 2
+label1:  addi s0, s0, 4
+         li   t0, 4
+         beq  s0, t0, end
+         addi s0, s0, 6
+end:     li x0, 1
---
+++ tests/integration-custom/in/bge-basic-no-taken.s
@@ -0,0 +1,7 @@
+         addi s1, x0, 1
+         addi s0, x0, -1
+         bge  s0, s1, label1
+         addi s0, s0, 2
+label1:  bge  x0, s0, end
+         addi s0, x0, 2
+end:     li x0, 1
